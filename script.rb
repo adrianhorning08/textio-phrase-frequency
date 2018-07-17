7721705bc788896264b2128da185d8bda1e11043
Adrian Horning <m.adrian.horning@gmail.com>
2018-07-17 16:31:56 -0700

    Kind of filters

--- script.rb
+++ script.rb
@@ -5,7 +5,7 @@ require 'pp'
 class MostCommonNGrams
 
   def initialize
-    @file_contents = File.open("easy.txt", "r").read
+    @file_contents = File.open("easier.txt", "r").read
     @phrases = Hash.new
     @sentences = []
   end
@@ -59,22 +59,84 @@ class MostCommonNGrams
 
   def n_grams(n, array)
     n_gram = array.each_cons(n).to_a
-    # print n_gram
     n_gram.each do |gram|
-      count_n_grams(gram.join(' '))
+      gram = gram.join(' ')
+      if @phrases.key?(gram)
+        @phrases[gram] += 1
+      else
+        @phrases[gram] = 1
+      end
     end
   end
 
-  def count_n_grams(n_gram)
-    if @phrases.key?(n_gram)
-      @phrases[n_gram] += 1
-    else
-      @phrases[n_gram] = 1
+  def filter_subsets
+    i = 0
+    phrase_keys = @phrases.keys
+    while i < phrase_keys.length - 1
+      j = i + 1
+      while j < phrase_keys.length
+        # puts "a: #{phrase_keys[i]} b: #{phrase_keys[j]}"
+        if is_subset?(phrase_keys[i], phrase_keys[j])
+          @phrases.delete(phrase_keys[i])
+          @phrases[phrase_keys[j]] += 1
+        end
+        j+=1
+      end
+      i+=1
     end
+
+  end
+
+  def count_n_grams(gram)
+
+    # You go through each key, and then see which one is longer
+    # then you see if the shorter is a subset of the longer
+    # if it is, delete both (or whichever is on the hash) and
+    # put the longer one on and
+
+    longer = gram
+    phrase_count = 1
+    @phrases.each_key do |key|
+      if is_subset?(key, gram)
+        phrase_count += @phrases[key]
+        @phrases.delete(key)
+        longer = longer(key, gram)
+      end
+    end
+    # if it gets thru the entire loop and and a similar
+    # key isn't found, it should add it to the hash
+    @phrases[longer] = phrase_count
+  end
+
+  def longer(phrase1, phrase2)
+    if phrase1.length > phrase2.length
+      longest = phrase1
+    else
+      longest = phrase2
+    end
+    longest
+  end
+
+  def is_subset?(phrase1, phrase2)
+    longest = longer(phrase1, phrase2)
+    shortest = phrase1 == longest ? phrase2 : phrase1
+
+    a = 0
+    b = 0
+
+    while a < shortest.length && b < longest.length
+      if shortest[a] != longest[b]
+        b += 1
+      elsif shortest[a] == longest[b]
+        a += 1
+        b += 1
+      end
+    end
+    a == shortest.length
   end
 
   def sort_phrases
-    # print @phrases.each_with_index.sort_by(&:first)
+    @phrases.sort {|a,b| b[1]<=>a[1]}
   end
 
   def run
@@ -82,8 +144,8 @@ class MostCommonNGrams
     split_sentences
     strip_leading_whitespace
     loop_thru_sentences
-    sort_phrases
-    pp @phrases
+    filter_subsets
+    print sort_phrases[0..10]
   end
 
 end
