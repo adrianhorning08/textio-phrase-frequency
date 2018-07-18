4edcb0642bcec60035e566a52142cadd0cfb1f2f
Adrian Horning <m.adrian.horning@gmail.com>
2018-07-18 13:29:06 -0700

    done

--- README.md
+++ README.md
@@ -15,3 +15,16 @@ In retaliation the quick brown fox jumped over ten snoring turtles.
 Example output
 
 ['the lazy dog', 'the quick brown fox jumped over']
+
+## Usage
+
+1. Clone repo `git clone https://github.com/adrianhorning08/textio-phrase-frequency.git`
+2. `cd textio-phrase-frequency`
+3. Enter a string into `@file_contents` or provide a file. ie `@file_contents = File.open("sample.txt", "r").read`
+3. Run `ruby script.rb`
+4. Results will output to terminal
+
+## Implementation
+Used Ruby because it is very easy to read contents of a file, and the methods Ruby provides abstracts many time consuming tasks, such as finding n grams and looping through a hash.
+
+I used a hash to store all n grams and their frequency. This becomes slow when I try to find if a phrase is a subset of another phrase (O(n^2)). Something like a prefix tree would perform much faster.
--- script.rb
+++ script.rb
@@ -1,13 +1,7 @@
-# loop through entire file, finding the end of sentences, then provide that
-# string to another method to split it to n-grams
-require 'pp'
-
-# prefix tree
-
 class MostCommonNGrams
 
   def initialize
-    @file_contents = File.open("sample.txt", "r").read
+    @file_contents = @file_contents = '' #input goes here
     @phrases = Hash.new
     @sentences = []
   end
@@ -25,14 +19,10 @@ class MostCommonNGrams
           temp += @file_contents[i].downcase
           i+=1
         end
-        @sentences << temp
+        @sentences << temp.strip
       end
       i+=1
     end
-  end
-
-  def strip_leading_whitespace
-    @sentences.map! { |e| e.strip }
   end
 
   def loop_thru_sentences
@@ -44,15 +34,12 @@ class MostCommonNGrams
   def split_into_n_grams(sentence)
     split_words = sentence.split(' ')
     sentence_length = split_words.length
-
     if sentence_length < 10
       max = sentence_length
     else
       max = 10
     end
-
     n = 3
-
     while n <= max
       n_grams(n, split_words)
       n += 1
@@ -60,6 +47,8 @@ class MostCommonNGrams
   end
 
   def n_grams(n, array)
+    # each_cons iterates over the given array n times, to_a will then return
+    # an array of n grams
     n_gram = array.each_cons(n).to_a
     n_gram.each do |gram|
       gram = gram.join(' ')
@@ -85,7 +74,6 @@ class MostCommonNGrams
     while i < phrase_keys.length - 1
       j = i + 1
       while j < phrase_keys.length
-        # puts "a: #{phrase_keys[i]} b: #{phrase_keys[j]}"
         if is_subset?(phrase_keys[i], phrase_keys[j])
           @phrases.delete(phrase_keys[i])
           @phrases[phrase_keys[j]] += 1
@@ -94,27 +82,6 @@ class MostCommonNGrams
       end
       i+=1
     end
-  end
-
-  def count_n_grams(gram)
-
-    # You go through each key, and then see which one is longer
-    # then you see if the shorter is a subset of the longer
-    # if it is, delete both (or whichever is on the hash) and
-    # put the longer one on and
-
-    longer = gram
-    phrase_count = 1
-    @phrases.each_key do |key|
-      if is_subset?(key, gram)
-        phrase_count += @phrases[key]
-        @phrases.delete(key)
-        longer = longer(key, gram)
-      end
-    end
-    # if it gets thru the entire loop and and a similar
-    # key isn't found, it should add it to the hash
-    @phrases[longer] = phrase_count
   end
 
   def longer(phrase1, phrase2)
@@ -129,10 +96,8 @@ class MostCommonNGrams
   def is_subset?(phrase1, phrase2)
     longest = longer(phrase1, phrase2)
     shortest = phrase1 == longest ? phrase2 : phrase1
-
     a = 0
     b = 0
-
     while a < shortest.length && b < longest.length
       if shortest[a] != longest[b]
         b += 1
@@ -165,11 +130,10 @@ class MostCommonNGrams
   def run
     strip_new_lines_and_punc
     split_sentences
-    strip_leading_whitespace
     loop_thru_sentences
     filter_non_repeats
     filter_subsets
-    puts return_top_10
+    print return_top_10
   end
 
 end
